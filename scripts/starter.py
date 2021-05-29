3c0d44152fbbf3007621037aa65e44677383429c
Arya Massarat <23412689+aryarm@users.noreply.github.com>
2021-05-29 14:57:22 -0700

    add X and y variables to the starter script

--- scripts/starter.py
+++ scripts/starter.py
@@ -6,17 +6,19 @@
 # data.object_name (ex: data.tpm or data.split())
 
 # This module creates the following variables:
-# tpm - A gene expression matrix for AD, N, and C patients
+# tpm - A gene expression matrix for AD, N, and C patients (rows = samples, columns = genes)
 # donors - Info about each individual in the study
-# genes - Info about each gene in tpm
+# genes - Info about each gene in the tpm matrix
 # data - The TPM matrix subsetted to AD and N patients. The last column will contain each patient's status as AD or N
+# X - the data matrix, but without its last column
+# y - the last column of the data matrix
 # X_train - raw gene expression matrix for training AD vs N patients
 # X_test - raw gene expression matrix for testing AD vs N patients
 # y_train - labels (1 for AD and 0 for N) corresponding to rows in X_train
 # y_test - labels (1 for AD and 0 for N) corresponding to rows in X_test
 
 # In addition, a function called split() is provided that can be used to update
-# the values of X_train, X_test, y_train, y_test
+# the values of X, y, X_train, X_test, y_train, y_test
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -46,17 +48,13 @@ data['group'].replace({'AD': 1, 'N': 0}, inplace=True)
 # define this code as a function so it can be called from outside the module, if needed
 # note that calling this function will change the values of X_train, X_test, y_train,
 # and y_test globally!
-def split(dat=None, test_size=0.33, random_state=42):
+def split(test_size=0.33, random_state=42):
     ''' create a test/train split '''
-    if dat is None:
-        global data
-        dat = data
-    global X_train, X_test, y_train, y_test
+    global data, X, y, X_train, X_test, y_train, y_test
+    X = data.loc[:, data.columns != 'group']
+    y = data['group']
     X_train, X_test, y_train, y_test = train_test_split(
-        dat.loc[:, dat.columns != 'group'],
-        dat['group'],
-        test_size=test_size,
-        random_state=random_state
+        X, y, test_size=test_size, random_state=random_state
     )
 
 # split dataset into training and testing
