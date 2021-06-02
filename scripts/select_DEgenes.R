60109f60cc055b252787e08eb36355ce5c3b2e41
Mike Cuoco <mcuoco12@gmail.com>
2021-06-01 19:14:34 -0700

    make compatible with bash output

--- scripts/select_DEgenes.R
+++ scripts/select_DEgenes.R
@@ -1,32 +1,34 @@
+#!/usr/bin/env Rscript
+
 # load packages
 library(reticulate) # interface with python
-library(DESeq2) # differential expression
+suppressPackageStartupMessages(library(DESeq2)) # differential expression
 
 #' select DE genes from an RNA-seq count matrix
 #' 
 #' @param count.matrix a samples by genes matrix of non-negative numbers
-#' @param conditions a set of sample labels for each row in count.matrix
+#' @param conditions a vector of sample labels for each row in count.matrix
 #' @return DE genes with adjusted p-value < 0.05
 
-select_DEgenes <- function(count.matrix, conditions){
-  # for testing
-  # start = import(module = "scripts.starter", as = "data")
-  # count.matrix = start$X
-  # conditions = start$y
-  
-  # create DESeq object
-  coldata = data.frame(condition = as.factor(conditions), row.names = rownames(count.matrix))
-  dds = DESeqDataSetFromMatrix(countData = round(t(count.matrix)), colData = coldata, design = ~ condition)
+# load training data
+start = import(module = "scripts.starter", as = "data")
+count.matrix = start$X_train
+conditions = start$y_train
 
-  # filter
-  keep <- rowSums(counts(dds)) >= 10
-  dds <- dds[keep,]
-  
-  # run
-  dds = DESeq(dds)
-  res = results(dds)
-  
-  # return DE genes
-  de.genes = na.omit(rownames(res)[res$padj < 0.05])
-  return(as.vector(de.genes))
-}
+# create DESeq object
+coldata = data.frame(condition = as.factor(conditions), row.names = rownames(count.matrix))
+dds = DESeqDataSetFromMatrix(countData = round(t(count.matrix)), colData = coldata, design = ~ condition)
+
+# remove genes with < 10 counts
+keep <- rowSums(counts(dds)) >= 10
+dds <- dds[keep,]
+
+# run
+dds = DESeq(dds)
+res = results(dds)
+
+# return DE genes
+de.genes = as.vector(na.omit(rownames(res)[res$padj < 0.05]))
+# start$data = start$data[,c(de.genes,"group")]
+cat(de.genes,sep=",")
+
