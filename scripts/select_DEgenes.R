700a5dd0fb88f574c5e49aa5007abd652dd14321
Mike Cuoco <mcuoco12@gmail.com>
2021-06-02 22:24:30 -0700

    add Ttest and file output

--- scripts/select_DEgenes.R
+++ scripts/select_DEgenes.R
@@ -2,7 +2,9 @@
 
 # load packages
 library(reticulate) # interface with python
+suppressPackageStartupMessages(library(tidyverse))
 suppressPackageStartupMessages(library(DESeq2)) # differential expression
+library(broom)
 
 #' select DE genes from an RNA-seq count matrix
 #' 
@@ -11,10 +13,13 @@ suppressPackageStartupMessages(library(DESeq2)) # differential expression
 
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args)==0) {
-  top_n = 195 # genes with p-values less than 0.05
-} else if (length(args)==1) {
+  # defaullt values
+  top_n = 100 
+  method = "Ttest"
+} else if (length(args)>0) {
   # default output file
   top_n = strtoi(args[1])
+  method = args[2]
 }
 
 # load training data
@@ -22,20 +27,49 @@ start = import(module = "scripts.starter", as = "data")
 count.matrix = start$X_train
 conditions = start$y_train
 
-# create DESeq object
-coldata = data.frame(condition = as.factor(conditions), row.names = rownames(count.matrix))
-dds = DESeqDataSetFromMatrix(countData = round(t(count.matrix)), colData = coldata, design = ~ condition)
+if (method == "Ttest"){
+  count.matrix = count.matrix %>% as.data.frame()
+  # perform t-test on each gene
+  test = map_df(count.matrix, function(.x){
+    case = .x[conditions==1]
+    control= .x[conditions==0]
+    out = t.test(case,control) %>% tidy()
+  })
+  
+  # wrangle
+  res = test %>%
+    mutate(gene = colnames(count.matrix)) %>%
+    filter(!is.nan(p.value)) %>%
+    mutate(padj = p.adjust(p.value, "fdr")) %>%
+    column_to_rownames("gene")
+  
+  # return DE genes
+  de.genes = as.vector(rownames(head(res[order(res$p.value),], top_n)))
+  con = file("DEgenes.txt")
+  writeLines(de.genes, con)
+  close(con)
+}
 
-# remove genes with < 10 counts
-keep <- rowSums(counts(dds)) >= 10
-dds <- dds[keep,]
+if (method ==  "DEseq"){
+  # create DESeq object
+  coldata = data.frame(condition = as.factor(conditions), row.names = rownames(count.matrix))
+  dds = DESeqDataSetFromMatrix(countData = round(t(count.matrix)), colData = coldata, design = ~ condition)
+  
+  # remove genes with < 10 counts
+  keep <- rowSums(counts(dds)) >= 10
+  dds <- dds[keep,]
+  
+  # run
+  dds = DESeq(dds)
+  res = results(dds)
+  
+  # return DE genes
+  #de.genes = as.vector(na.omit(rownames(res)[res$padj < 0.05])) # get genes with p-value less than 0.05, only
+  de.genes = as.vector(rownames(head(res[order(res$padj),], top_n)))
+  con = file("DEgenes.txt")
+  writeLines(de.genes, con)
+  close(con)
+}
 
-# run
-dds = DESeq(dds)
-res = results(dds)
 
-# return DE genes
-de.genes = as.vector(na.omit(rownames(res)[res$padj < 0.05])) # get genes with p-value less than 0.05, only
-de.genes = as.vector(rownames(head(res[order(res$padj),], top_n)))
-cat(de.genes,sep=",")
 
